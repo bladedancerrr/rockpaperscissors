dbb6199425b855f28e6965c812d950f67ba46664
Ayesha Ahmed <32429595+bladedancer-256@users.noreply.github.com>
2020-05-04 16:13:48 +1000

    Fixed formatting getUserChoice.ino

--- getUserChoice.ino
+++ getUserChoice.ino
@@ -1,32 +1,20 @@
 //reads user's input from bend sensors
-
-
 char getUserChoice(){
+  
+  char userChoice;
+  int sensor1 = digitalRead(sensor1);
+  int sensor2 = digitalRead(sensor2);
+  
+  if(sensor1 == LOW && sensor2 == LOW){
+    userChoice='p';
+  }
 
-char userChoice;
+  else if (sensor1 == LOW && sensor2 == HIGH){
+    userChoice='s';
+  }
 
-int sensor1 = digitalRead(sensor1);
-
-int sensor2 = digitalRead(sensor2);
-
-if(sensor1 == LOW && sensor2 == LOW){
-
-userChoice='p';
-
-}
-
-else if (sensor1 == LOW && sensor2 == HIGH){
-
-userChoice='s';
-
-}
-
-else if(sensor1 == HIGH && sensor2 == HIGH){
-
-userChoice='r';
-
-}
-
-return userChoice;
-
+  else if(sensor1 == HIGH && sensor2 == HIGH){
+    userChoice='r';
+  }
+  return userChoice;
 }
